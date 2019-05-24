64c6d8f5e5de4949497d59842a41d4e9faeba060
Chandra Krintz <ckrintz@yahoo.com>
2019-05-24 13:06:09 -0700

    initial commit

---
+++ AWS/sns.py
@@ -0,0 +1,34 @@
+import json
+import boto3
+
+#########################
+if __name__ == "__main__":
+    main()
+
+#########################
+def main():
+    parser = argparse.ArgumentParser(description='post a message to a topic in AWS SNS. It assumes that your credentials are in the correct place for fracelab profile')
+    parser.add_argument('arn',action='store',help='Topic ARN')
+    parser.add_argument('msg',action='store',type=str,help='SNS message')
+    parser.add_argument('sub',action='store',type=str,help='SNS subject')
+    args = parser.parse_args()
+    arn = args.arn
+    msg = args.msg
+    sub = args.sub
+
+    return post(arn,msg,sub)
+
+#########################
+def post(topic,msg,subject):
+
+    message = {"foo": "bar"}
+    boto3.setup_default_session(profile_name='racelab')
+    client = boto3.client('sns',region_name='us-west-2')
+    response = client.publish(
+        TargetArn=topic,
+        Message=json.dumps({'default': json.dumps(message),
+                            'email': msg}),
+        Subject=subject,
+        MessageStructure='json'
+    )
+    return response
